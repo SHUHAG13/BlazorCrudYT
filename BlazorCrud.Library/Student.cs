281ebb2cb9e2fad00ab1d13ff00f2bf3c4c4d8d0
shuhag <shuhag200113@gmail.com>
2025-01-09 13:25:21 +0600

    update

--- BlazorCrud.Library/Student.cs
+++ BlazorCrud.Library/Student.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,10 +10,17 @@ namespace BlazorCrud.Library
 	public class Student
 	{
         public int Id { get; set; }
+        [Required(ErrorMessage ="Name is required")]
         public string Name { get; set; }
+        [Required(ErrorMessage = "Father Name is required")]
         public string FatherName { get; set; }
+        [Required(ErrorMessage = "Mobile number is required")]
+        [Range(1,9)]
         public string MobileNumber { get; set; }
+        [Required(ErrorMessage = "Email is required")]
+        [EmailAddress(ErrorMessage ="Invalid email")]
         public string Email { get; set; }
+        [Required(ErrorMessage = "Roll num is required")]
         public int RollNumber { get; set; }
     }
 }
